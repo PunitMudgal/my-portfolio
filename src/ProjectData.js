5f21b2425e90bb1020457cb3fdbfbf60df2d7c88
Punit Mudgal <ssmudgal01@gmail.com>
2023-01-29 22:19:41 +0530

    docs: added link

--- src/ProjectData.js
+++ src/ProjectData.js
@@ -25,14 +25,14 @@ const ProjectData = [
         projectNo: 4,
         ProjectName: 'Text Format',
         Description: 'In this app you can copy, paste your text, know the word count, bold it, Remove the Extra spaces, etc.',
-        SourceCode: '',
+        SourceCode: 'https://github.com/PunitMudgal/textutils',
         VisitPro: 'https://textformatter114.netlify.app'
     },
     {
         projectNo: 5,
         ProjectName: 'Tik Tak Toe',
         Description: 'This is a working tik tak toe game with rules and dark mode. You can play it for time pass and fun. The rules of this game are given in about secrion.',
-        SourceCode: '',
+        SourceCode: 'https://github.com/PunitMudgal/tik-tak-toe',
         VisitPro: 'https://tik-tak-toe-114.netlify.app'
     }
 ]
